eb928e83008199c11941c4599cbaa4851462c1d4
RobinzonGit <rarobertg@gmail.com>
2026-04-18 22:51:01 +0500

    Добавьте файлы проекта.

---
+++ TODOList2/TODOitem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TODOList2
+{
+    public class TODOitem
+    {
+        public int ID {  get; set; }
+
+        public string Title { get; set; }
+
+        public bool DONE { get; set; }
+
+        public override bool Equals(object? other)
+        {
+            return this.Title.Equals((other as TODOitem).Title);
+        }
+    }
+}
