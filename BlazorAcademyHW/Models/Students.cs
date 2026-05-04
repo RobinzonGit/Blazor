cc2fb58b781a43f39f23696304697c8b2042ba28
RobinzonGit <rarobertg@gmail.com>
2026-05-04 11:56:32 +0500

    Убрал коментарии в Студентах и Учителях

--- BlazorAcademyHW/Models/Students.cs
+++ BlazorAcademyHW/Models/Students.cs
@@ -2,37 +2,6 @@
 
 namespace BlazorAcademyHW.Models
 {
-    //public class Students
-    //{
-    //    public int Id { get; set; }
-
-    //    [Required(ErrorMessage = "Last Name is required")]
-    //    //[Column("last_name")]
-    //    public string LastName { get; set; }
-
-    //    [Required(ErrorMessage = "First Name is required")]
-    //    //[Column("first_Name")]
-    //    public string FirstName { get; set; }
-
-    //    //[Column("middle_name")]
-    //    public string MiddleName { get; set; }
-
-    //    //[Column("birth_date")]
-    //    public DateTime DateOfBirth { get; set; }
-
-    //    //[Column("email")]
-    //    public string Email { get; set; }
-
-    //    //[Column("phone")]
-    //    public string Phone { get; set; }
-
-    //    //[Column("photo")]
-    //    public string Photo { get; set; }
-
-    //    //[Column("rate")]
-    //    public int Group { get; set; }
-    //}
-
     public class Students
     {
         public int Id { get; set; }
--- BlazorAcademyHW/Models/Teachers.cs
+++ BlazorAcademyHW/Models/Teachers.cs
@@ -5,32 +5,6 @@ namespace BlazorAcademyHW.Models
 {
     public class Teachers
     {
-        //public int Id { get; set; }
-
-        //[Required(ErrorMessage = "Last Name is required")]
-        ////[Column("last_name")]
-        //public string LastName { get; set; }
-
-        //[Required(ErrorMessage = "First Name is required")]
-        ////[Column("first_Name")]
-        //public string FirstName { get; set; }
-
-        ////[Column("middle_name")]
-        //public string MiddleName { get; set; }
-
-        ////[Column("birth_date")]
-        //public DateTime DateOfBirth { get; set; }
-
-        ////[Column("email")]
-        //public string Email { get; set; }
-
-        ////[Column("phone")]
-        //public string Phone { get; set; }
-
-        ////[Column("photo")]
-        //public string Photo { get; set; }
-
-        //[Column("work_since")]
         public int Id { get; set; }
         [Required] public string LastName { get; set; } = string.Empty;
         [Required] public string FirstName { get; set; } = string.Empty;
