f68c76d6697331fc381e406d3ab357e7fb7eb1e5
RobinzonGit <rarobertg@gmail.com>
2026-05-12 13:01:14 +0500

    Исправление ошибки в Students

--- BlazorAcademyHW/Models/Students.cs
+++ BlazorAcademyHW/Models/Students.cs
@@ -6,26 +6,17 @@ namespace BlazorAcademyHW.Models
     public class Students
     {
         public int Id { get; set; }
-
-        [Required]
-        
-        public string LastName { get; set; } = string.Empty;
-
-        [Required]
-        public string FirstName { get; set; } = string.Empty;
-
+        [Required] public string LastName { get; set; } = string.Empty;
+        [Required] public string FirstName { get; set; } = string.Empty;
         public string? MiddleName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public byte[]? Photo { get; set; }
-
         public int? GroupId { get; set; }
-
         [ForeignKey(nameof(GroupId))]
         public virtual Groups? Group { get; set; }
 
-        // Вычисляемое свойство, не сохраняется в БД
         [NotMapped]
         public int Age
         {
@@ -38,5 +29,11 @@ namespace BlazorAcademyHW.Models
                 return age;
             }
         }
+
+        [NotMapped]
+        public string? DirectionName => Group?.Direction?.Name;
+
+        [NotMapped]
+        public string? GroupName => Group?.Name;
     }
 }
